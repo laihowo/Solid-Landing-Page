5ff36578b6be0917d1860c8e21e9d0e7b5662012
Benny LAI <howo0707@gmail.com>
2020-11-12 16:32:24 +0800

    Built production.

---
+++ build/precache-manifest.c498b090414bca900a5db6192f94b917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d4f8d9eb89b71004cde117381758a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "573b3696f4601b9bcc2e",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1def542df3adcc3e1d37",
+    "url": "/static/js/2.ef9b312c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ef9b312c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "573b3696f4601b9bcc2e",
+    "url": "/static/js/main.9ebc4d46.chunk.js"
+  },
+  {
+    "revision": "e22fd322f0b45aec7fb4",
+    "url": "/static/js/runtime-main.a7f6340d.js"
+  }
+]);
